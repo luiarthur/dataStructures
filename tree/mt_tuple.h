823c9ce6bad5cb97719fb221d1ba95aef216a056
luiarthur <luiarthur@gmail.com>
2016-04-03 22:38:09 -0700

    yes

--- tree/mt_tuple.h
+++ tree/mt_tuple.h
@@ -9,11 +9,16 @@ struct MTTuple {
   
   MTTuple() : d(-1), x(0), t(0) {}
   MTTuple(int dd, double xx, double tt) : d(dd), x(xx), t(tt) {}
+
+  // to_string()
+  virtual std::string to_string() {
+    std::ostringstream os;
+    os << d << "," << x << "," << t;
+    return "(" + os.str() + ")";
+  }
 };
 
-// operator "<<" overloading for MTTuple
+// operator "<<" overloading for MTTuple => string: (d,x,t)
 std::ostream& operator<<(std::ostream& out, MTTuple& tup) {
-  std::ostringstream os;
-  os << tup.d << "," << tup.x << "," << tup.t;
-  return out << "(" + os.str() + ")";
+  return out << tup.to_string();
 }
